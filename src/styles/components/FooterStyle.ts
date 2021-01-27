05438034e22dc3c9d88fb61dfda84b84c51fd34b
aXenDev <58148176+aXenDeveloper@users.noreply.github.com>
2021-01-27 14:04:05 +0100

    Add footer

--- src/styles/components/FooterStyle.ts
+++ src/styles/components/FooterStyle.ts
@@ -1,5 +1,40 @@
 import styled from 'styled-components';
+import { Container } from '../layout';
 
-export const FooterStyle = styled.footer`
-	margin-top: 2rem;
+export const FooterStyle = styled.footer``;
+
+export const FooterSVGStyle = styled.svg`
+	display: block;
+	width: 100%;
+	height: 130px;
+	transform: rotate(180deg);
+	margin-bottom: -0.5rem;
+
+	@media screen and (max-width: 767px) {
+		height: 40px;
+	}
+
+	path {
+		fill: #fff;
+	}
+`;
+
+export const FooterBackground = styled.div`
+	background-color: #fff;
+`;
+
+export const ContainerFooter = styled(Container)`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	@media screen and (max-width: 767px) {
+		justify-content: center;
+		flex-direction: column;
+	}
+	padding: 2rem 1.5rem;
+
+	a {
+		font-weight: bold;
+	}
 `;
